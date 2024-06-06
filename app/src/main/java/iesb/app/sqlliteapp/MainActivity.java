108fb4b1bbef5e24a7881e8f6954b720d7db3175
MarceloCPJunior <marcelocastro.pj@gmail.com>
2024-06-05 21:05:06 -0300

    Criação

--- app/src/main/java/iesb/app/sqlliteapp/MainActivity.java
+++ app/src/main/java/iesb/app/sqlliteapp/MainActivity.java
@@ -13,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
@@ -21,37 +22,12 @@ import iesb.app.sqlliteapp.model.ClienteVO;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText pesquisa;
-    private TextView nomePesquisa;
-    private TextView emailPesquisa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        /*
-        pesquisa = findViewById(R.id.pesquisa);
-        nomePesquisa = findViewById(R.id.nomePesquisado);
-        emailPesquisa = findViewById(R.id.emailPesquisado);*/
-        pesquisa.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) { }
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
-            @Override
-            public void afterTextChanged(Editable s) {
-                if(s.toString().length() > 4) {
-                    loadCadastroLayout(s.toString());
-                }
-            }
-        });
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 
     public void btnOnClickCadastrarCliente(View view) {
@@ -67,45 +43,28 @@ public class MainActivity extends AppCompatActivity {
         db.addCliente(vo);
     }
 
-    public void loadCadastroLayout(String nome) {
-        ClienteDAO db = new ClienteDAO(this);
-
-        ClienteVO cliente = db.getClienteNome(nome);
-
-        if(cliente != null) {
-            nomePesquisa.setText(cliente.getNome());
-            emailPesquisa.setText(cliente.getEmail());
-        }
+    public void btnOnClickCadastro(View view) {
+        setContentView(R.layout.cadastro_cliente);
     }
-    /*
-    <EditText
-    android:id="@+id/pesquisa"
-    android:layout_width="wrap_content"
-    android:layout_height="wrap_content"
-    android:ems="10"
-    android:inputType="text"
-    android:hint="Pesquisar"
-    tools:layout_editor_absoluteX="43dp"
-    tools:layout_editor_absoluteY="59dp" />
 
-    <TextView
-    android:id="@+id/nomePesquisado"
-    android:layout_width="wrap_content"
-    android:layout_height="wrap_content"
-    android:ems="10"
-    android:inputType="text"
-    android:hint="Nome Pesquisado"
-    tools:layout_editor_absoluteX="43dp"
-    tools:layout_editor_absoluteY="59dp" />
+    public void btnOnClickListar(View view) {
+        ClienteDAO db = new ClienteDAO(this);
+        RecyclerView listaCliente = findViewById(R.id.lista);
 
-    <TextView
-    android:id="@+id/emailPesquisado"
-    android:layout_width="wrap_content"
-    android:layout_height="wrap_content"
-    android:ems="10"
-    android:inputType="text"
-    android:hint="Email Pesquisado"
-    tools:layout_editor_absoluteX="43dp"
-    tools:layout_editor_absoluteY="59dp" />*/
+        setContentView(R.layout.listar_cliente);
+
+    }
+
+    public void btnOnClickMenu(View view) {
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnOnClickDeletar(View view) {
+        setContentView(R.layout.deletar_cliente);
+    }
+
+    public void btnOnClickEditar(View view) {
+        setContentView(R.layout.deletar_cliente);
+    }
 
 }
